6dfe35294a75706cef2fef7711a1befdf31de456
Dang Minh Tien <dangminhtien06@gmail.com>
2019-10-06 00:42:27 +0700

    Add Webpack + env + Graphql

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+require('dotenv').config()
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();

9dcf73395ba27819f5a15395d5f9ec2057fbf9f5
Dang Minh Tien <dangminhtien06@gmail.com>
2019-10-16 21:37:43 +0700

    Add Typeorm mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { MainModule } from './modules/main.module'
+import { getMetadataArgsStorage } from 'typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+require('dotenv').config()
 
 @Module({
   imports: [
     MainModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGO_URL || 'mongodb://localhost:27017/karaoke',
+      entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
+      synchronize: true,
+      useUnifiedTopology: true
+    }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       definitions: {
